6d7138f118f3e3bb438e5b80dc4f762dff92155b
MaciejWojs <misiek111403@gmail.com>
2024-02-26 19:53:22 +0100

    Zaj2 - zadania

---
+++ zaj2/zaj2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <limits>
+using string = std::string;
+
+
+
+class kalkualtor{
+	private:
+	int liczba1, liczba2;
+	public:
+	kalkualtor(): liczba1(0), liczba2(0){};
+	~kalkualtor(){
+		std::cout<<"Destruktor\n";
+	}
+
+	int dodawanie(){
+		return this->liczba1+liczba2;
+	}
+	int odejmowanie(){
+		return this->liczba1-liczba2;
+	}
+	int mnozenie(){
+		return this->liczba1*liczba2;
+	}
+	int klawiatura(){
+		std::cout<<Podaj pierwsza liczbe;
+	}
+}
+
+int main() {
+
+kalkualtor calc;
+}
