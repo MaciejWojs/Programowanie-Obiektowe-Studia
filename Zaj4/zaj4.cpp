83d61e89abc6d8cbc41fd1f5eba8337c6303beea
MaciejWojs <misiek111403@gmail.com>
2024-03-12 21:41:24 +0100

    zaj4.cpp - zad3

--- Zaj4/zaj4.cpp
+++ Zaj4/zaj4.cpp
@@ -13,14 +13,14 @@ void zad5();
 void zad6();
 
 int main() {
-    zad2();
+    zad3();
     /*
-        zad1();
-        zad3();
-        zad4();
-        zad5();
-        zad6();
-        */
+    zad1();
+    zad2();
+    zad4();
+    zad5();
+    zad6();
+    */
     return 0;
 }
 
@@ -238,4 +238,129 @@ void zad2() {
 
     moc tv;
     std::cout << "Moc TV to: " << tv.get_wat() << "W" << std::endl;
+}
+
+void zad3() {
+    /*
+    Program ma za zadanie wyliczenie kosztu zużycia paliwa.W metodzie „wylicz” należy użyć
+    takiego wzoru : „(zuzycie / 100)* cena”.Poniżej jest podana klasa koszt.Uzupełnij jej metody aby
+    program działał.W tej klasie widzimy dwa konstruktory.Pierwszy jest domyślny a drugi jest
+    parametrowy.Gdy popatrzymy na pierwszy konstruktor widzimy po dwukropku wywołanie
+    konstruktora parametrowego z dwoma parametrami(w tym wypadku 0 i 0).Taki zapis nazwany jest
+    „delegowaniem konstruktora”.Gdy w funkcji main użyjemy konstruktora bez parametrowego
+    program wywoła konstruktor parametrowy z wpisanymi wartościami(w tym wypadku 0 i 0).
+    Kod klasy „koszt” :
+
+    class koszt {
+        private:
+        double zuzycie;
+        double cena;
+
+        public:
+        //konstruktor 1 domyslny
+        koszt() : koszt(0.0, 0.0) {}
+        //konstruktor 2 parametrowy
+        koszt(double x, double y) : zuzycie(x), cena(y) {}
+        ~koszt(void) {}
+        void set_zuzycie(double x);
+        void set_cena(double x);
+        double get_zuzycie(void);
+        double get_cena(void);
+        double wylicz(void);
+    };
+
+    Uzupełnij metody o ciało
+
+    Funkcja main :
+    int main(int argc, char** argv) {
+        double z;
+        double c;
+        cout << "Policzmy koszt jednego kilometra dla Opel: " << endl;
+        cout << "Podaj zuzycie paliwa: ";
+        cin >> z;
+        cout << "Podaj cene paliwa: ";
+        cin >> c;
+        koszt opel; //konstruktor domyslny
+        cout << "Koszt: " << opel.wylicz() << " zl. za km" << endl; //(1)
+        opel.set_zuzycie(z);
+        opel.set_cena(c);
+        cout << "jeszcze raz koszt: " << opel.wylicz() << " zl. za km" << endl << endl; //(2)
+        cout << "Policzmy koszt jednego kilometra dla VW: " << endl;
+        cout << "Podaj zuzycie paliwa: ";
+        cin >> z;
+        cout << "Podaj cene paliwa: ";
+        cin >> c;
+        koszt vw(z, c); //konstruktor parametrowy
+        cout << "Koszt: " << vw.wylicz() << " zl. za km" << endl;
+    }
+
+    Utworzyliśmy dwa obiekty. Pierwszy to opel a drugi to vw. Zobacz że w linijce „koszt opel;” zostaje
+    wywołany konstruktor bezparametrowy, a w deklaracji klasy tam mamy delegowanie konstruktora.
+    Czyli wywołanie konstruktora parametrowego z zerami. W linijce która ma w komentarzu(1)
+    wyświetli nam 0 (wartości domyślne).W linijce gdzie w komentarzu mamy(2) widzimy już wyliczony
+    koszt. W obiekcie vw mamy od razu wpisane dane więc od razu jest koszt wyliczany.
+    */
+
+    class koszt {
+        private:
+        double zuzycie;
+        double cena;
+
+        public:
+        //konstruktor 1 domyslny
+        koszt() : koszt(0.0, 0.0) {
+            std::cout << "konstruktor 1 domyslny\n\n";
+
+        }
+        //konstruktor 2 parametrowy
+        koszt(double x, double y) : zuzycie(x), cena(y) {
+            std::cout << "konstruktor 2 parametrowy\n\n";
+        }
+
+        ~koszt(void) {
+            std::cout << "\n\nDekonstruktor";
+        }
+
+        void set_zuzycie(double x) {
+            zuzycie = x;
+        };
+
+        void set_cena(double x) {
+            cena = x;
+        };
+
+        double get_zuzycie(void) {
+            return zuzycie;
+        };
+
+        double get_cena(void) {
+            return cena;
+        };
+
+        double wylicz(void) {
+            return (zuzycie / 100) * cena;
+        };
+    };
+
+    double z;
+    double c;
+    std::cout << "Policzmy koszt jednego kilometra dla Opel: " << std::endl;
+    std::cout << "Podaj zuzycie paliwa: ";
+    std::cin >> z;
+    std::cout << "Podaj cene paliwa: ";
+    std::cin >> c;
+    koszt opel; //konstruktor domyslny
+    std::cout << "Koszt: " << opel.wylicz() << " zl. za km" << std::endl; //(1)
+    opel.set_zuzycie(z);
+    opel.set_cena(c);
+    std::cout << "jeszcze raz koszt: " << opel.wylicz() << " zl. za km" << std::endl << std::endl; //(2)
+
+    std::cout << "Policzmy koszt jednego kilometra dla VW: " << std::endl;
+    std::cout << "Podaj zuzycie paliwa: ";
+    std::cin >> z;
+    std::cout << "Podaj cene paliwa: ";
+    std::cin >> c;
+
+    koszt vw(z, c); //konstruktor parametrowy
+    std::cout << "Koszt: " << vw.wylicz() << " zl. za km" << std::endl;
 }
