fdb5f37fbb3190aee6aefa018fd2e7039a8899f1
MaciejWojs <misiek111403@gmail.com>
2024-03-20 10:50:42 +0100

    Zaj5.cpp - zad2

--- Zaj5/Zaj5.cpp
+++ Zaj5/Zaj5.cpp
@@ -1,6 +1,7 @@
 // TODO NA KOLOKWIUM nauczyc się UML
 
 #include <iostream>
+using string = std::string;
 void zad1();
 void zad2();
 void zad3();
@@ -10,9 +11,9 @@ void zad6();
 
 int main() {
   std::cout << "cokolwiek";
-  zad1();
-  /*
   zad2();
+  /*
+  zad1();
   zad3();
   zad4();
   zad5();
@@ -156,7 +157,111 @@ void zad1() {
   std::cout << "siostra wyliczyla ilosc samochodow w rodzinie: " << dla_siostry.get_ilosc() << std::endl; //3
 }
 
-void zad2();
+class rodzina {
+private:
+  string imie;
+  static int konto;
+public:
+  rodzina(string x) : imie(x) {}
+  ~rodzina(void) {
+    std::cout << "\nDekonstruktor";
+  }
+  string get_imie(void) {
+    return imie;
+  };
+  void wyplata(int x) {
+    konto += x;
+  };
+  void zakupy(int x) {
+    konto -= x;
+  };
+  int stan_konta(void) {
+    return konto;
+  };
+};
+
+int rodzina::konto = 1000;
+
+void zad2() {
+  /*
+  Napisz program który będzie sumował dochody oraz wydatki pieniędzy. Klasa nazywa się
+  rodzina. Każdy nowy obiekt jest członkiem rodziny i ma swoje imię. Jednakże mamy zmienną
+  statyczną „konto” która przetrzymuje stan konta całej rodziny. Na początku programu zainicjalizuj
+  zmienną „konto” kwotą 1000zł. Dopisz ciała metod brakujących do klasy poniżej
+
+  class rodzina
+  {
+  private:
+   string imie;
+   static int konto;
+  public:
+   rodzina(string x): imie(x) {}
+   ~rodzina(void) {}
+   string get_imie(void);
+   void wyplata(int x);
+   void zakupy(int x);
+   int stan_konta(void);
+  };
+
+  Przetestujmy działanie programu:
+  int main(int argc, char** argv) {
+    rodzina tata("Adam");
+    tata.wyplata(2000);
+    std::cout<<"Tata ma na imie "<<tata.get_imie()<<std::endl;
+    rodzina mama("Renata");
+    std::cout<<"Mama ma na imie "<<mama.get_imie()<<std::endl;
+    mama.wyplata(2000);
+    rodzina dziecko("Robert");
+    std::cout<<"Dziecko ma na imie "<<dziecko.get_imie()<<std::endl;
+
+    std::cout<<"Mamy na wspolnym koncie bankowym "<<mama.stan_konta()<<"zl"<<std::endl;
+
+    std::cout<<"Zakupy za 550zl"<<std::endl;
+    mama.zakupy(550);
+    std::cout<<"Mamy na wspolnym koncie bankowym "<<mama.stan_konta()<<"zl"<<std::endl;
+
+    std::cout<<"Zakupy za 110zl"<<std::endl;
+    mama.zakupy(110);
+    std::cout<<"Mamy na wspolnym koncie bankowym "<<mama.stan_konta()<<"zl"<<std::endl;
+
+    std::cout<<"Zakupy za 110"<<std::endl;
+    tata.zakupy(110);
+    std::cout<<"Mamy na wspolnym koncie bankowym "<<tata.stan_konta()<<"zl"<<std::endl;
+
+    std::cout<<"Zakupy za 50"<<std::endl;
+    dziecko.zakupy(110);
+    std::cout<<"Mamy na wspolnym koncie bankowym "<<dziecko.stan_konta()<<"zl"<<std::endl;
+  }
+  */
+
+  rodzina tata("Adam");
+  tata.wyplata(2000);
+  std::cout << "Tata ma na imie " << tata.get_imie() << std::endl;
+  rodzina mama("Renata");
+  std::cout << "Mama ma na imie " << mama.get_imie() << std::endl;
+  mama.wyplata(2000);
+  rodzina dziecko("Robert");
+  std::cout << "Dziecko ma na imie " << dziecko.get_imie() << std::endl;
+
+  std::cout << "Mamy na wspolnym koncie bankowym " << mama.stan_konta() << "zl" << std::endl;
+
+  std::cout << "Zakupy za 550zl" << std::endl;
+  mama.zakupy(550);
+  std::cout << "Mamy na wspolnym koncie bankowym " << mama.stan_konta() << "zl" << std::endl;
+
+  std::cout << "Zakupy za 110zl" << std::endl;
+  mama.zakupy(110);
+  std::cout << "Mamy na wspolnym koncie bankowym " << mama.stan_konta() << "zl" << std::endl;
+
+  std::cout << "Zakupy za 110" << std::endl;
+  tata.zakupy(110);
+  std::cout << "Mamy na wspolnym koncie bankowym " << tata.stan_konta() << "zl" << std::endl;
+
+  std::cout << "Zakupy za 50" << std::endl;
+  dziecko.zakupy(110);
+  std::cout << "Mamy na wspolnym koncie bankowym " << dziecko.stan_konta() << "zl" << std::endl;
+};
+
 void zad3();
 void zad4();
 void zad5();
