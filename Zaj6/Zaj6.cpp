9046d1bb4e377da982f9255023b403db14a680a6
MaciejWojs <misiek111403@gmail.com>
2024-04-05 08:19:35 +0200

    Zaj6.cpp - zad2

--- Zaj6/Zaj6.cpp
+++ Zaj6/Zaj6.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+using string = std::string;
 
 void zad1();
 void zad2();
@@ -9,9 +10,9 @@ void zad5();
 void zad6();
 
 int main() {
-  zad1();
-  /*
   zad2();
+  /*
+  zad1();
   zad3();
   zad4();
   zad5();
@@ -57,4 +58,64 @@ void zad1() {
 
   std::cout << "osoba: " << o1.getImie() << ' ' << o1.getNazwisko() << '\n';
   std::cout << "osoba: " << o2.getImie() << ' ' << o2.getNazwisko() << '\n';
+}
+
+class student {
+private:
+  string imie;
+  int ocena;
+public:
+  student(void) : imie("-*-"), ocena(0) {}   //konstruktor 1 
+  student(string a) : imie(a), ocena(0) {}   //konstruktor 2 
+  student(string a, int b) : imie(a), ocena(b) {}  //konstruktor 3 
+  ~student(void) {}
+
+  void set_imie(string a) {
+    imie = a;
+  }
+
+  void set_ocena(int a) {
+    ocena = a;
+  }
+
+  string get_imie(void) {
+    return imie;
+  }
+
+  int get_ocena(void) {
+    return ocena;
+  }
+
+  void wyswietl(void);
+};
+
+void student::wyswietl(void) {
+  std::cout << "imie: " << imie << ", ocena: " << ocena << std::endl;
+}
+
+void zad2() {
+  student klasa_I[2];
+  for (int i = 0; i < 2; i++)
+    klasa_I[i].wyswietl();
+
+  std::cout << std::endl << std::endl;
+
+  student klasa_II[4] = { student("Marek"), student("Jurek",4) };
+  for (int i = 0; i < 4; i++) {
+    klasa_II[i].wyswietl();
+  }
+
+  std::cout << std::endl << "poprawiamy ocene Markowi na 5" << std::endl;
+  klasa_II[0].set_ocena(5);
+  klasa_II[0].wyswietl();
+
+  /*
+  Tabela, pozycja konstruktor
+Tabela: klasa_I, pozycja: 0        konstruktor 1
+Tabela: klasa_I, pozycja: 1        konstruktor 1
+Tabela: klasa_II, pozycja: 0       konstruktor 2
+Tabela: klasa_II, pozycja: 1       konstruktor 3
+Tabela: klasa_II, pozycja: 2       konstruktor 1
+Tabela: klasa_II, pozycja: 3       konstruktor 1
+  */
 }
