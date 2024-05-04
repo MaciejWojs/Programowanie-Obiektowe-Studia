2d6048998e0b3c9c9505cc56138dbb5d5d32a598
MaciejWojs <misiek111403@gmail.com>
2024-05-04 13:34:19 +0200

    Zaj10.cpp - zad2

--- Zaj10/Zaj10.cpp
+++ Zaj10/Zaj10.cpp
@@ -8,9 +8,9 @@ void zad5();
 void zad6();
 
 int main() {
-  zad1();
+  zad2();
   /*
-    zad2();
+    zad1();
     zad3();
     zad4();
     zad5();
@@ -41,3 +41,68 @@ void zad1() {
   a.funkcja();
 }
 
+class liczba {
+private:
+  int liczba1;
+public:
+  liczba(void) : liczba(0) {};
+  liczba(int z) : liczba1(z) {};
+  ~liczba(void) {};
+
+  void wczytaj(int z) {
+    std::cout << "Podaj liczbe: ";
+    std::cin >> liczba1;
+  }
+
+  int wypisz(void) {
+    return liczba1;
+  }
+};
+
+class operacja {
+private:
+  liczba* a;
+  liczba* b;
+public:
+  operacja() {
+    a = NULL;
+    b = NULL;
+  }
+  operacja(liczba* x, liczba* y) : a(x), b(y) {};
+  ~operacja(void) {
+    if (a != NULL) delete a;
+    if (b != NULL) delete b;
+  }
+
+  void ustaw1(int z) {
+    *a = z;
+  }
+
+  void ustaw2(int z) {
+    *b = z;
+  }
+
+  int druk1(void) {
+    return a->wypisz();
+  }
+
+  int druk2(void) {
+    return b->wypisz();
+  }
+
+  int dodaj(void) {
+    if (a->wypisz() > 0 && b->wypisz() > 0)
+      return a->wypisz() + b->wypisz();
+    else
+      return -1;
+  }
+};
+
+void zad2() {
+  operacja B(new liczba, new liczba(5));
+  B.ustaw1(-4);
+  std::cout << "liczba 1: " << B.druk1() << std::endl;
+  std::cout << "liczba 2: " << B.druk2() << std::endl;
+
+  std::cout << "suma liczb " << B.dodaj() << std::endl;
+}
