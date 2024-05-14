9e3a9377296349168430f872acc36bd9d86954a1
MaciejWojs <misiek111403@gmail.com>
2024-05-14 19:41:02 +0200

    Zaj12.cpp - zad3

--- Zaj12/Zaj12.cpp
+++ Zaj12/Zaj12.cpp
@@ -9,13 +9,13 @@ void zad4();
 void zad5();
 
 int main() {
-  zad2();
   /*
   zad1();
-  zad3();
+  zad2();
   zad4();
   zad5();
   */
+  zad3();
 }
 
 class figura {
@@ -185,11 +185,12 @@ public:
 
 void zad2() {
   osoba* osoby[5] = {
-  new pracownik("Jan","Kowalski","dr.",2000),
-  new student("Oskar","Kwiatkowski",10012,"informatyka"),
-  new student("Mieczyslaw","Sroka",10014,"informatyka"),
-  new student("Pawel","Baran",10015,"informatyka"),
-  new student("Patrycja","Biel",10018,"informatyka") };
+    new pracownik("Jan","Kowalski","dr.",2000),
+    new student("Oskar","Kwiatkowski",10012,"informatyka"),
+    new student("Mieczyslaw","Sroka",10014,"informatyka"),
+    new student("Pawel","Baran",10015,"informatyka"),
+    new student("Patrycja","Biel",10018,"informatyka")
+  };
 
   for (int i = 0;i < 5;i++) {
     osoby[i]->wyswietl();
@@ -199,4 +200,86 @@ void zad2() {
   for (int i = 4;i >= 0;i--) {
     delete osoby[i];
   }
+}
+
+class figura2 {
+private:
+  int a;
+public:
+  figura2(int x) : a(x) {};
+  ~figura2(void) {};
+
+  void setBokA(int x) {
+    a = x;;
+  }
+
+  int getBokA(void) {
+    return a;
+  }
+
+  virtual void rysuj(void) = 0;
+};
+
+class kwadrat2 : public figura2 {
+public:
+  kwadrat2(int x) : figura2(x) {};
+  ~kwadrat2(void) {};
+
+  virtual void rysuj(void) {
+    for (int i = 0; i < getBokA(); i++) {
+      for (int j = 0; j < getBokA(); j++) {
+        if (j == 0 || j == getBokA() - 1 || i == 0 || i == getBokA() - 1) {
+          std::cout << '*';
+        } else {
+          std::cout << ' ';
+        }
+      }
+      std::cout << std::endl;
+    }
+
+  }
+};
+
+class prostokat2 : public figura2 {
+private:
+  int b;
+public:
+  prostokat2(int x, int y) : figura2(x), b(y) {};
+  ~prostokat2(void) {};
+
+  void setBokB(int x) {
+    b = x;
+  };
+
+  int  getBokB(void) {
+    return b;
+  }
+
+  virtual void rysuj(void) {
+    for (int i = 0; i < getBokA(); i++) {
+      for (int j = 0; j < getBokB(); j++) {
+        if (j == 0 || j == getBokB() - 1 || i == 0 || i == getBokA() - 1) {
+          std::cout << '*';
+        } else {
+          std::cout << ' ';
+        }
+      }
+      std::cout << std::endl;
+    }
+
+  }
+};
+
+void zad3() {
+  kwadrat2 kw(5);
+  kw.rysuj();
+  std::cout << std::endl << std::endl;
+
+  prostokat2 pr(5, 7);
+  pr.rysuj();
+  std::cout << std::endl << std::endl;
+
+  figura2* f;
+  f = &kw;
+  f->rysuj();
 }
